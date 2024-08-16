7d76f4224a301bd5629b7ad7cc219e7ed9af8874
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2024-08-16 12:56:11 +0200

    Updated smart navbar

---
+++ Scripts/populate-navbar.js
@@ -0,0 +1,12 @@
+fetch('/navbar.html')
+.then(response => response.text())
+.then(data => {
+  document.getElementById('navbar').innerHTML = data;
+  const currentPage = window.location.pathname.split('/')[1];
+  if (currentPage) {
+    document.getElementById(currentPage).classList.add('currentlink');
+  } else {
+    document.getElementById('home').classList.add('currentlink');
+  }
+});
+
