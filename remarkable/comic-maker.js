bda585a554fe1fb3d52ddba27f4d57194d0c9162
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2025-11-01 22:26:17 +0530

    Comic 25!

--- remarkable/comic-maker.js
+++ remarkable/comic-maker.js
@@ -11,7 +11,7 @@ const params = new URLSearchParams(window.location.search);
 const rawoutput = params.get("num");
 let desiredID = parseInt(rawoutput);
 
-const maxComicID = 24;
+const maxComicID = 25;
 let currentComicID;
 
 if (isNaN(desiredID) || desiredID < 1 || desiredID > maxComicID) {
